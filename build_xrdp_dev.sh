841f51b52ba19a794f90098088d829ca52fce391
Ernad Husremovic <hernad@bring.out.ba>
2015-12-07 15:41:40 +0100

    cache

--- build_xrdp_dev.sh
+++ build_xrdp_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp .dockerignore.build .dockerignore
-docker build --no-cache -f Dockerfile.build -t build_xrdp .
+docker build -f Dockerfile.build -t build_xrdp .
 
 if [ "$1" == "bash" ]; then
   cmd=/bin/bash
