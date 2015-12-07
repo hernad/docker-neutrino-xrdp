cf50920f22e3ae69725c5aa4c6e858a7edfe77c5
Ernad Husremovic <hernad@bring.out.ba>
2015-12-07 13:12:26 +0100

    /data/conf/browser, /data/mp3

--- xrdp_run.sh
+++ xrdp_run.sh
@@ -5,7 +5,8 @@ docker rm -f neutrino-xrdp
 docker run \
   --rm -ti \
   -p 3389:3389 \
-  -v $(pwd)/mp3:/root/mp3 \
+  -v $(pwd)/mp3:/data/mp3 \
+  -v $(pwd)/conf/browser:/data/conf/browser \
   --name neutrino-xrdp \
   neutrino-xrdp 
 
