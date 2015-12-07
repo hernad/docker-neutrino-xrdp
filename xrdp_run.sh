f08b25a53048b43e225caba936c8e15fbc18759c
Ernad Husremovic <hernad@bring.out.ba>
2015-12-07 11:49:30 +0100

    xinitrc, google chrome

--- xrdp_run.sh
+++ xrdp_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+docker rm -f neutrino-xrdp
+
 docker run \
   --rm -ti \
   -p 3389:3389 \
   -v $(pwd)/mp3:/root/mp3 \
   --name neutrino-xrdp \
-  neutrino-xrdp \
-  /bin/bash
+  neutrino-xrdp 
 
