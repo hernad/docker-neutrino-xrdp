3585f293cc04d4ca470b2b0cbc2ffed843ecef91
Ernad Husremovic <hernad@bring.out.ba>
2016-06-17 16:35:29 +0200

    update

--- build_xrdp_dev.sh
+++ build_xrdp_dev.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 CT=neutrino-xrdp-dev
-IMG=neutrino-xrdp-dev
-
-cp .dockerignore.build .dockerignore
-docker build -f Dockerfile.build -t $IMG .
+IMG=hernad/neutrino-xrdp-dev
 
 if [ "$1" == "bash" ]; then
   cmd="/bin/bash"
@@ -14,13 +11,22 @@ else
   cmd_switch=""
 fi
 
+if [ "$1" == "--no-build" ]; then
+  echo no build
+  cmd="/no_build.sh"
+else
+  echo build
+  cp .dockerignore.build .dockerignore
+  docker build -f Dockerfile.build -t $IMG .
+fi
+
+
 [ ! -d $(pwd)/build/xrdp ] && mkdir -p $(pwd)/build/xrdp
 [ ! -d $(pwd)/build/X11rdp ] && mkdir -p $(pwd)/build/X11rdp
 
 docker rm -f $CT
 
-docker run $cmd_switch --name $CT \
-   $IMG $cmd
+docker run $cmd_switch --name $CT $IMG $cmd
 
 #   -v $(pwd)/build/xrdp:/opt/xrdp \
 #   -v $(pwd)/build/X11rdp:/opt/X11rdp \
---
+++ no_build.sh
@@ -0,0 +1,30 @@
+cd /opt/xrdp/sesman/chansrv/pulse 
+sed -i -e 's/PULSE_DIR = .*$/PULSE_DIR = \/root\/pulseaudio-4.0/' Makefile 
+make 
+tar cvfz /opt/pulseaudio_module-xrdp.tar.gz  module-xrdp-*.so
+
+    
+mkdir -p /opt/X11rdp/xrdp_etc
+mkdir -p /opt/X11rdp/xrdp_etc/pam.d
+mkdir -p /opt/X11rdp/xrdp_etc/init.d
+
+cd /opt/xrdp
+make install 
+xrdp-keygen xrdp auto && \
+    cp instfiles/pam.d/xrdp-sesman.other /opt/X11rdp/xrdp_etc/pam.d/xrdp-sesman
+
+cd /opt/xrdp/xorg/X11R7.6 && \
+    ./buildx.sh /opt/X11rdp 
+
+ln -s /opt/X11rdp/bin/X11rdp /usr/local/bin/X11rdp && \
+cp /etc/xrdp/xrdp* /opt/X11rdp/xrdp_etc/init.d/
+
+
+cd /usr/local
+tar -czf /opt/usr_local.tar.gz .
+
+cd /etc
+tar -czf /opt/etc_xrdp.tar.gz xrdp
+
+cd /opt
+tar -czf /opt/X11rdp.tar.gz --exclude="*/test/*" --exclude="*.a" X11rdp
